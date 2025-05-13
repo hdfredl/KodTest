d334cba4b6c4fdbd959da645244622fb0f0e11fe
Fredrik Luu <fredrik.luu@cgi.com>
2025-05-13 10:39:02 +0200

    Add project files.

---
+++ KodTest/Losning/Losning.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace KodTest.Alternativ1
+{
+    public class Losning
+    {
+        public void EnLosning(int[] fig)
+        {
+            if (fig != null)
+            {
+                if (fig[0] == fig[1] && fig.Length < 3)
+                {
+                    Kvadrat(fig);
+                }
+
+                if (fig.Length == 4)
+                {
+                    Rektangel(fig);
+                }
+
+                if (fig.Length > 4)
+                {
+                    Paralelltrapets(fig);
+                }
+            }
+        }
+
+
+        void Kvadrat(int[] langd)
+        {
+            if (langd == null || langd.Length == 0)
+            {
+                WriteLine("Ingen tal i arrayen");
+            }
+
+            if (langd.Length == 2 && langd[0] == langd[1])
+            {
+                int area = langd[0] * langd[1];
+                int omkrets = langd[0] + langd[1] + langd[0] + langd[1];
+
+                WriteLine("KVADRAT");
+                WriteLine("Arean är: " + area);
+                WriteLine("Omkretsen är: " + omkrets);
+            }
+            else
+            {
+                WriteLine("Tyvärr kan denna array inte bli en kvadrat");
+            }
+        }
+
+        void Rektangel(int[] langd)
+        {
+            if (langd == null || langd.Length == 0)
+            {
+                WriteLine("Ingen tal i arrayen");
+            }
+
+            if (langd[0] == langd[2] && langd[1] == langd[3] || langd[0] == langd[1] && langd[2] == langd[3])
+            {
+                if (langd[0] != langd[1])
+                {
+                    int area = langd[0] * langd[1];
+
+                    WriteLine("REKTANGEL");
+                    WriteLine("Area är: " + area);
+                    OmkretsRaknare(langd);
+                }
+                else if (langd[2] != langd[3])
+                {
+                    int area = langd[2] * langd[3];
+
+                    WriteLine("REKTANGEL");
+                    WriteLine("Area är: " + area);
+                    OmkretsRaknare(langd);
+                }
+                else if (langd[0] == langd[1] || langd[2] == langd[3])
+                {
+                    int area = langd[2] * langd[3];
+
+                    WriteLine("REKTANGEL");
+                    WriteLine("Area är: " + area);
+                    OmkretsRaknare(langd);
+                }
+            }
+            else
+            {
+                WriteLine("Tyvärr kan inte dessa längder bilda en rektangel");
+            }
+        }
+
+        void Paralelltrapets(int[] langd)
+        {
+            decimal tak = langd[2]; // 4
+            decimal bas = langd[1]; // 5
+            decimal hojd = langd[4]; // 3
+
+            decimal area = hojd * (bas + tak) / 2;
+
+            int omkrets = langd[0] + langd[1] + langd[2] + langd[3] + langd[4];
+
+            WriteLine("PARALELLTRAPETS");
+            WriteLine("Area är: " + area);
+            WriteLine("Omkrets är: " + omkrets);
+
+            WriteLine("Figur3 kan ej bli paralleltrapets om vi använder oss av siffrorna i arrayen. Det går att skriva ut talen i en trapets och räkna Area/Omkrets men ej att rita i verkligheten... ");
+        }
+
+        void Parallelogram(int[] langd)
+        {
+            int area = langd[4] * langd[2];
+            int omkrets = langd[0] + langd[1] + langd[2] + langd[3];
+
+            WriteLine("PARALLELLOGRAM");
+            WriteLine("Figur3 kan bli en paralellogram om vi använder oss av siffror i arrayen.  ");
+            WriteLine("Paralellogram var ej efterfrågat i uppgiften. Men fick ej till det med beräkningen med de siffror som finns med i arrayen.");
+            WriteLine(langd[4] + " = höjd," + langd[2] + " = bas");
+            WriteLine("Area är: " + area);
+            WriteLine("Omkrets är: " + omkrets);
+        }
+
+        void WriteLine(string input)
+        {
+            Console.WriteLine(input);
+        }
+
+        void OmkretsRaknare(int[] langd)
+        {
+            int omkrets = langd[0] + langd[1] + langd[2] + langd[3];
+            WriteLine("Omkrets är: " + omkrets);
+        }
+    }
+}
---
+++ KodTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using KodTest.Alternativ1;
+
+namespace KodTest
+{
+    /// Beräkna arean o omkretsen på en kvadrat, rektangel och parallelltrapets
+    /// Skriv ut arean, omkretsen och vilken typ av figur det är, skriv ut i consolen
+    /// Jag har skapat 3 arrayer, arrayen avgör vilken typ figur det är
+    /// Var kreativ och visa att du testat din lösning noga
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] figur1 = new int[] { 5, 5 };
+            int[] figur2 = new int[] { 5, 4, 5, 4 };
+            int[] figur3 = new int[] { 5, 5, 4, 4, 3 };
+
+            Console.ReadLine();
+
+            int[] figur = figur2;
+
+            var losningen = new Losning();
+            losningen.EnLosning(figur);
+
+
+        }
+    }
+}
